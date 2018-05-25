e2f1fd8f9e7f6970f4f9edda0ee818007b959cca
Jhoneagle <32411534+Jhoneagle@users.noreply.github.com>
2018-05-25 14:35:31 +0300

    csv parser

---
+++ parseMap.py
@@ -0,0 +1,15 @@
+import sys, os
+
+if __name__ == "__main__":
+	path = sys.argv[1]
+	
+	newPath = "parsed.csv"
+	newF = open(newPath, "w", encoding="utf-8")
+	
+	with open(path, "r", encoding="utf-8") as f:
+		for line in f:
+			parse = line.split('","')
+			cleaned = '"' + parse[1] + '","' + parse[8] + '","' + parse[18] + '","' + parse[19] + '"' + "\n"
+			newF.write(str(cleaned))
+	
+	newF.close()
